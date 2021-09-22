2802d5a33f4f267a3f16aed5471148cf38701141
Ben Shushu <runninglinuxkernel@126.com>
2021-09-22 21:08:01 +0800

    内嵌汇编实验5: 实现读和写系统寄存器的宏
    Signed-off-by: Ben Shushu <runninglinuxkernel@126.com>

--- benos/include/io.h
+++ benos/include/io.h
@@ -36,4 +36,29 @@ static inline void delay(unsigned int n)
 		;
 }
 
+
+/*
+ * 在带参数的宏，#号作为一个预处理运算符,
+ * 可以把记号转换成字符串
+ *
+ * 下面这句话会在预编译阶段变成：
+ *  asm volatile("csrr %0, " "reg" : "=r" (__val)); __val; });
+ */
+#define read_csr(csr)						\
+({								\
+	register unsigned long __v;				\
+	__asm__ __volatile__ ("csrr %0, " #csr			\
+			      : "=r" (__v) :			\
+			      : "memory");			\
+	__v;							\
+})
+
+#define write_csr(val, csr)					\
+({								\
+	unsigned long __v = (unsigned long)(val);		\
+	__asm__ __volatile__ ("csrw " #csr ", %0"		\
+			      : : "rK" (__v)			\
+			      : "memory");			\
+})
+
 #endif

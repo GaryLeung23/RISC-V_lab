39ebd89d2d95a1d93bd0260ff57a427c86fbc367
Ben Shushu <runninglinuxkernel@126.com>
2022-10-19 20:34:18 +0800

    指令集5：条件比较指令
    Signed-off-by: Ben Shushu <runninglinuxkernel@126.com>

--- benos/src/kernel.c
+++ benos/src/kernel.c
@@ -6,13 +6,30 @@ extern void shift_test(void);
 extern void add_sub_test(void);
 extern void compare_test(void);
 
+extern unsigned long compare_and_return(unsigned long a, unsigned long b);
+
 void asm_test(void)
 {
-    load_store_test();
-    pc_related_test();
-    shift_test();
-    add_sub_test();
-    compare_test();
+	unsigned long val1, val2;
+
+	load_store_test();
+	pc_related_test();
+	shift_test();
+	add_sub_test();
+	compare_test();
+
+	val1 = compare_and_return(10, 9);
+       if (val1 == 0)
+               uart_send_string("compare_and_return ok\n");
+       else 
+               uart_send_string("compare_and_return fail\n");
+
+       val2 = compare_and_return(9, 10);
+       if (val2 == 0xffffffffffffffff)
+               uart_send_string("compare_and_return ok\n");
+       else
+	       uart_send_string("compare_and_return fail\n");
+
 }
 
 void kernel_main(void)

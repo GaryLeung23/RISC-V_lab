9f916da9719aa2ca6e9c8ee808f40b3313acb519
Ben Shushu <runninglinuxkernel@126.com>
2022-11-20 20:31:39 +0800

    函数调用规范与栈1：栈的布局
    Signed-off-by: Ben Shushu <runninglinuxkernel@126.com>

--- benos/src/kernel.c
+++ benos/src/kernel.c
@@ -1,70 +1,17 @@
-#include "uart.h"
-#include "type.h"
-#include "memset.h"
-#include "printk.h"
-
-extern void load_store_test(void);
-extern void pc_related_test(void);
-extern void shift_test(void);
-extern void add_sub_test(void);
-extern void compare_test(void);
-
-extern unsigned long compare_and_return(unsigned long a, unsigned long b);
-extern unsigned long sel_test(unsigned long a, unsigned long b);
-extern void my_memcpy_test(void);
-extern void branch_test(void);
-
-void asm_test(void)
+int add_c(int a, int b)
 {
-	unsigned long val1, val2;
+	return a + b;
+}
 
-	load_store_test();
-	pc_related_test();
-	shift_test();
-	add_sub_test();
-	compare_test();
+int func1(void)
+{
+	int a = 1;
+	int b = 2;
 
-	val1 = compare_and_return(10, 9);
-       if (val1 == 0)
-               printk("compare_and_return ok\n");
-       else 
-               printk("compare_and_return fail\n");
-
-       val2 = compare_and_return(9, 10);
-       if (val2 == 0xffffffffffffffff)
-               printk("compare_and_return ok\n");
-       else
-	       printk("compare_and_return fail\n");
-
-       val1 = sel_test(0, 9);
-       if (val1 == 11)
-	       printk("sel test ok\n");
-
-       val2 = sel_test(5, 2);
-       if (val2 == 1)
-	       printk("sel test ok\n");
-
-       my_memcpy_test();
-       memset((void *)0x80210005, 0x55, 40);
-       memset((void *)0x80210045, 0x55, 18);
-       memset((void *)0x80210085, 0x55, 16+11);
-       memset((void *)0x802100c5, 0x55, 8);
-       memset((void *)0x80220005, 0x55, 80);
-
-       branch_test();
-       printk("branch test done\n");
+	return add_c(a, b);
 }
 
 void kernel_main(void)
 {
-	uart_init();
-	uart_send_string("Welcome RISC-V!\r\n");
-	init_printk_done();
-	printk("printk init done\n");
-
-    asm_test();
-
-    while (1) {
-        ;
-    }
+	func1();
 }

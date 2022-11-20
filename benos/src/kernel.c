afc9cf5be79a27afce1562f6a166d5bf2e4b6917
Ben Shushu <runninglinuxkernel@126.com>
2022-11-20 20:38:53 +0800

    Revert "函数调用规范与栈1：栈的布局"
    This reverts commit dcb54ca2c1b3cf81005ba40eb3207e3f2704dfe2.

--- benos/src/kernel.c
+++ benos/src/kernel.c
@@ -1,17 +1,63 @@
-int add_c(int a, int b)
-{
-	return a + b;
-}
+#include "uart.h"
+#include "type.h"
+#include "memset.h"
+#include "printk.h"
 
-int func1(void)
-{
-	int a = 1;
-	int b = 2;
+extern void load_store_test(void);
+extern void pc_related_test(void);
+extern void shift_test(void);
+extern void add_sub_test(void);
+extern unsigned long compare_and_return(unsigned long a, unsigned long b);
+extern unsigned long sel_test(unsigned long a, unsigned long b);
+extern void my_memcpy_test(void);
+extern void branch_test(void);
 
-	return add_c(a, b);
+void asm_test(void)
+{
+	unsigned long val1, val2;
+
+	load_store_test();
+	pc_related_test();
+	shift_test();
+	add_sub_test();
+
+	val1 = compare_and_return(10, 9);
+       if (val1 == 0)
+               printk("compare_and_return ok\n");
+       else 
+               printk("compare_and_return fail\n");
+
+       val2 = compare_and_return(9, 10);
+       if (val2 == 0xffffffffffffffff)
+               printk("compare_and_return ok\n");
+       else
+	       printk("compare_and_return fail\n");
+
+       val1 = sel_test(0, 9);
+       if (val1 == 11)
+	       printk("sel test ok\n");
+
+       val2 = sel_test(5, 2);
+       if (val2 == 1)
+	       printk("sel test ok\n");
+
+       my_memcpy_test();
+       memset((void *)0x80210005, 0x55, 40);
+
+       branch_test();
+       printk("branch test done\n");
 }
 
 void kernel_main(void)
 {
-	func1();
+	uart_init();
+	uart_send_string("Welcome RISC-V!\r\n");
+	init_printk_done();
+	printk("printk init done\n");
+
+	asm_test();
+
+	while (1) {
+		;
+	}
 }

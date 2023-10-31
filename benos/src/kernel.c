fc9ffce8ef6b344ba146d901b03c1f6657c68c4e
GaryLeung <819421619@qq.com>
2023-10-31 13:02:36 +0000

    指令集4-2：比较指令

--- benos/src/kernel.c
+++ benos/src/kernel.c
@@ -4,23 +4,25 @@ extern void load_store_test(void);
 extern void pc_related_test(void);
 extern void shift_test(void);
 extern void add_sub_test(void);
+extern void compare_test(void);
 
 void asm_test(void)
 {
-	load_store_test();
-	pc_related_test();
-	shift_test();
-	add_sub_test();
+    load_store_test();
+    pc_related_test();
+    shift_test();
+    add_sub_test();
+    compare_test();
 }
 
 void kernel_main(void)
 {
-	uart_init();
-	uart_send_string("Welcome RISC-V!\r\n");
+    uart_init();
+    uart_send_string("Welcome RISC-V!\r\n");
 
-	asm_test();
+    asm_test();
 
-	while (1) {
-		;
-	}
+    while (1) {
+        ;
+    }
 }
